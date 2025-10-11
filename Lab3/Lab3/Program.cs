2cb37ebbaca35b3a2dbd5e88eee0cd459c3aca45
Szymon Gerszendorf <sgerszendorf@edu.cdv.pl>
2025-10-11 14:25:19 +0200

    Zadania 4-6

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,24 @@
+﻿//4. Klasa i obiekty
+
+using System.Reflection.Metadata;
+
+var osoba1 = new Osoba("Konrad");
+var osoba2 = new Osoba("Kuba", )
+var osoba3 = new Osoba("Jacek")
+var osoba4 = new Osoba("Iwona")
+class Osoba
+{
+    private string imie;
+    private int wiek;
+    public Osoba(string imie, int wiek)
+    {
+       this.imie = imie;
+       this.wiek = wiek;
+    }
+
+    public void PrzedstawSie()
+    {
+        Console.WriteLine($"Nazywam się {imie} i mam {wiek}");
+    }
+
+}
