05a760bdcfb3fccc68d7031000b8ace6574aee43
Szymon Gerszendorf <sgerszendorf@edu.cdv.pl>
2025-10-11 13:40:31 +0200

    Zdadanie 1-3

--- Lab1.1/Lab1.1/Program.cs
+++ Lab1.1/Lab1.1/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//1. Pętla do...while
+/*string userPasword;
+do
+{
+    Console.WriteLine("Podaj hasło: ");
+    userPasword = Console.ReadLine();
+}
+while (userPasword != "admin123");
+Console.WriteLine("Zalogowano pomyślnie!");*/
 
-Console.WriteLine("Hello, World!");
+//2. Pętla while
+/*
+int Number;
+
+do
+{
+    Console.WriteLine("Podaj liczbę większą od 0: ");
+    Number = int.Parse(Console.ReadLine());
+
+} while (Number <= 0);
+    Console.WriteLine("Poprawna liczba!");
+    */
+
+//3. Pętla foreach
+/*string[] citys = { "Poznań", "Wrocław", "Warszawa", "Gdynia", "Zakopane" };
+foreach (string city in citys)
+{
+    Console.WriteLine($"Miasta: {city}");
+}*/
